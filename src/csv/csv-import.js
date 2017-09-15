8f9eecf323599c3bc8613b011402e3a9fc41391a
Pascal Chorus <p.chorus@me.com>
2017-09-15 20:24:40 +0200

    feat(csv-import): add rxjs example for csv import

---
+++ src/csv/csv-import.js
@@ -0,0 +1,51 @@
+const Rx = require('rxjs');
+const Observable = Rx.Observable;
+
+function createObjectsFromCsvByObservable(csv) {
+  const csvLines$ = Observable.from(csv.split('\n'));
+
+  // Create Observable for property names.
+  let properties$ = csvLines$
+    .first()
+    .mergeMap(val => val.split(';'));
+
+  // Map each data line of file to an object.
+  return csvLines$
+    .skip(1)
+    .map(val => val.split(';'))
+    .mergeMap(val =>
+      Observable.zip(properties$, val)
+        .reduce((acc, curr) => Object.assign(acc, { [curr[0]]: curr[1] }), {}));
+}
+
+function createObjectsFromCsvByLoop(csv) {
+  const result = [];
+  const csvLines = csv.split('\n');
+
+  const propertyNames = csvLines[0].split(';');
+
+  for (let i = 1; i < csvLines.length; ++i) {
+    const propertyValues = csvLines[i].split(';');
+    result.push(createObject(propertyNames, propertyValues));
+  }
+
+  return result;
+}
+
+function createObject(propNames, propValues) {
+  if (propNames.length === propValues.length) {
+    let obj = {};
+
+    for (let i = 0; i < propNames.length; ++i) {
+      obj[propNames[i]] = propValues[i];
+    }
+    return obj;
+  }
+}
+
+const str = `Prop1;Prop2;Prop3
+1;2;3
+A;B;C`;
+
+createObjectsFromCsvByObservable(str).subscribe(obj => console.log(obj));
+console.log(createObjectsFromCsvByLoop(str));

cb14bb5b2ad5167335f316c1a9c96353a0b1c1cf
Pascal Chorus <pascal.chorus@moneymeets.com>
2020-10-09 21:27:39 +0200

    feat(operators): add marble tests for combination operators

--- src/operators/combination-operators.ts
+++ src/operators/combination-operators.ts
@@ -1,5 +1,89 @@
 import { combineLatest, forkJoin, interval, of, timer, zip } from 'rxjs';
 import { map, take, tap, withLatestFrom } from 'rxjs/operators';
+import { TestScheduler } from 'rxjs/testing';
+
+const createTestScheduler = () =>
+  new TestScheduler((actual, expected) => expect(actual).toEqual(expected));
+
+describe('combination operator marble test', () => {
+  it('combineLatest combines latest values of multiple streams', () => {
+    const testScheduler = createTestScheduler();
+    testScheduler.run(({ cold, expectObservable }) => {
+      const a$ = cold(' a---b---(c|)', { a: 'A0', b: 'A1', c: 'A2' });
+      const b$ = cold(' a---b---(c|)', { a: 'B0', b: 'B1', c: 'B2' });
+      const expected = 'v---(wx)(yz|)';
+      const values = {
+        v: ['A0', 'B0'],
+        w: ['A1', 'B0'],
+        x: ['A1', 'B1'],
+        y: ['A2', 'B1'],
+        z: ['A2', 'B2'],
+      };
+      expectObservable(combineLatest([a$, b$])).toBe(expected, values);
+    });
+  });
+
+  it('combineLatest for synchronous observables combines latest values of multiple streams', () => {
+    const testScheduler = createTestScheduler();
+    testScheduler.run(({ cold, expectObservable }) => {
+      const a$ = cold(' a-b-(c|)', { a: 'A0', b: 'A1', c: 'A2' });
+      const b$ = cold(' ---------a---b---(c|)', {
+        a: 'B0',
+        b: 'B1',
+        c: 'B2',
+      });
+      const expected = '---------x---y---(z|)';
+      const values = {
+        x: ['A2', 'B0'],
+        y: ['A2', 'B1'],
+        z: ['A2', 'B2'],
+      };
+      expectObservable(combineLatest([a$, b$])).toBe(expected, values);
+    });
+  });
+
+  it('forkJoin combines last values of multiple streams', () => {
+    const testScheduler = createTestScheduler();
+    testScheduler.run(({ cold, expectObservable }) => {
+      const a$ = cold(' a-b---(c|)');
+      const b$ = cold(' x--y-(z|)');
+      const expected = '------(r|)';
+      const values = {
+        r: ['c', 'z'],
+      };
+      expectObservable(forkJoin([a$, b$])).toBe(expected, values);
+    });
+  });
+
+  it('zip combines values of multiple streams as long as every stream has values and no stream is completed', () => {
+    const testScheduler = createTestScheduler();
+    testScheduler.run(({ cold, expectObservable }) => {
+      const a$ = cold(' a-----(b|)');
+      const b$ = cold(' -i-j-(k|)');
+      const expected = '-x----(y|)';
+      const values = {
+        x: ['a', 'i'],
+        y: ['b', 'j'],
+      };
+      expectObservable(zip(a$, b$)).toBe(expected, values);
+    });
+  });
+
+  it('withLatestFrom combines observable values with latest value of second observable', () => {
+    const testScheduler = createTestScheduler();
+    testScheduler.run(({ cold, expectObservable }) => {
+      const a$ = cold(' a-----b---c-(d|)');
+      const b$ = cold(' --i-j---(k|)');
+      const expected = '------x---y-(z|)';
+      const values = {
+        x: ['b', 'j'],
+        y: ['c', 'k'],
+        z: ['d', 'k'],
+      };
+      expectObservable(a$.pipe(withLatestFrom(b$))).toBe(expected, values);
+    });
+  });
+});
 
 describe('combination operator', () => {
   it('combineLatest combines latest values of multiple streams', done => {
